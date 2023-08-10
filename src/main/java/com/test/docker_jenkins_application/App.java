8ba981cbfb4aca5df4167e5045a23d7167e959fb
swap30 <swapnilpotdar913@gmail.com>
2023-08-10 10:21:57 +0530

    first commit

---
+++ src/main/java/com/test/docker_jenkins_application/App.java
@@ -0,0 +1,13 @@
+package com.test.docker_jenkins_application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
